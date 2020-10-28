47aec27ba59e4b7062e43bd5ea07fd37b1c36b58
czqu <ryan.h.qin@outlook.com>
2020-10-28 15:47:58 +0800

    规范化

--- _source/assets/js/118.js
+++ _source/assets/js/118.js
@@ -1,1 +1,4 @@
-$(function(){$("#footer").load("footer.html")});try{window.console&&window.console.log&&console.log("%c别看了，快来投递你的简历 zhaoxin@2084team.cn","color:red")}catch(e){};
+
+$(function() {
+$('#footer').load('footer.html');
+})
